548cb6f9cfa277e463518c2a59aee2cde7d4566f
Sahil Anand <sanand@sfsu.edu>
2026-06-02 22:13:02 -0700

    Added MySQL DB

--- csc317-code-sahilanxnd/application/conf/database.js
+++ csc317-code-sahilanxnd/application/conf/database.js
@@ -1,13 +1,17 @@
 var mysql = require('mysql2');
-// Create a MySQL connection pool using environment variables and export it as a promise-based pool
+
 const pool = mysql.createPool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    waitForConnections: true,
-    connectTimeout: 15000,
-    connectionLimit: 20
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
+  ssl: {
+    rejectUnauthorized: true
+  },
+  waitForConnections: true,
+  connectTimeout: 15000,
+  connectionLimit: 20
 });
 
 module.exports = pool.promise();
