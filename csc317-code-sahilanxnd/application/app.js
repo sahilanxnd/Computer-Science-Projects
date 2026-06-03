bba586976c47c3a4510a0f859004f43d97f1dbc0
Sahil Anand <sanand@sfsu.edu>
2026-06-02 22:45:54 -0700

    Fixed MySQL connection

--- csc317-code-sahilanxnd/application/app.js
+++ csc317-code-sahilanxnd/application/app.js
@@ -25,10 +25,16 @@ app.engine(
   })
 );
 
-const sessionStore = new MySQLStore(
-  {/*default option*/ },
-  require('./conf/database')
-);
+const sessionStore = new MySQLStore({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
 
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
--- csc317-code-sahilanxnd/application/conf/database.js
+++ csc317-code-sahilanxnd/application/conf/database.js
@@ -7,7 +7,7 @@ const pool = mysql.createPool({
   database: process.env.DB_NAME,
   port: process.env.DB_PORT,
   ssl: {
-    rejectUnauthorized: true
+    rejectUnauthorized: false
   },
   waitForConnections: true,
   connectTimeout: 15000,
